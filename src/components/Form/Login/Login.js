6fcd08bc904243a535ca68891c4ddf49fe0c4db9
mutai-gilbert <mutaigilbert85@gmail.com>
2023-10-05 22:20:34 +0300

    Added the login features

--- src/components/Form/Login/Login.js
+++ src/components/Form/Login/Login.js
@@ -1,2 +1,38 @@
-const [loggedUsername, setLoggedUsername] = useState("");
-const [loggedEmail, setLoggedEmail] = useState("");
+import React from 'react'
+import { useState } from 'react';
+
+const Login = () => {
+    const [loginUsername, setLoginUsername] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+  return (
+    <div>
+        <h1>LOgin</h1>
+
+<form>
+  <label>
+    Username:
+    <input
+      type="text"
+      name="name"
+      value={loginUsername}
+      onChange={(e) => setLoginUsername(e.target.value)}
+    />
+  </label>
+
+  <label>
+    Password:
+    <input
+      type="text"
+      name="password"
+      value={loginPassword}
+      onChange={(e) => setLoginPassword(e.target.value)}
+    />
+  </label>
+  <button onClick={submitLogin}>Submit</button>
+</form>
+      
+    </div>
+  )
+}
+
+export default Login
