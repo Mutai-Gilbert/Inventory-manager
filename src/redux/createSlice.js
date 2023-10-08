2b9580403170922139d1d8aeca4eaa383cc7aa02
mutai-gilbert <mutaigilbert85@gmail.com>
2023-10-08 22:11:26 +0300

    Added some documentation on the code

--- src/redux/createSlice.js
+++ src/redux/createSlice.js
@@ -5,6 +5,20 @@ const initialState = {
     data: null,
     error: null,
 };
+/**
+ * The formReducer function handles different actions related to submitting a form and updates the
+ * state accordingly.
+ * @param [state] - The `state` parameter represents the current state of the form in the Redux store.
+ * It is an object that contains properties such as `loading`, `data`, and `error`.
+ * @param action - The `action` parameter represents the action object that is dispatched to the
+ * reducer. It contains information about the action type and any additional data that is needed to
+ * update the state. In this case, the action object should have a `type` property that corresponds to
+ * one of the `actionTypes` defined
+ * @returns The formReducer function returns a new state object based on the action type. If the action
+ * type is "submitFormRequest", it returns a new state object with the loading property set to true and
+ * the data and error properties set to null. If the action type is "submitFormSuccess", it returns a
+ * new state object with the loading property set to false, the data property set to the action payload
+ */
 
 const formReducer = (state = initialState, action) => {
     switch (action.type) {
