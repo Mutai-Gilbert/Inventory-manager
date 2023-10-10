ce968d354a8dd2a62b9f1509f57d488073987722
mutai-gilbert <mutaigilbert85@gmail.com>
2023-10-10 22:35:27 +0300

    Added some documentation on the code

--- src/components/ActionsTypes/actionTypes.js
+++ src/components/ActionsTypes/actionTypes.js
@@ -1,3 +1,6 @@
+/* These lines of code are exporting three constants: `submitFormRequest`, `submitFormSuccess`, and
+`submitFormFailure`. These constants are typically used as action types in Redux applications to
+define the different stages of a form submission process. */
 export const submitFormRequest = 'submitFormRequest';
 export const submitFormSucess = 'submitFormSuccess';
 export const submitFormFailure = 'submitFormFailure';
